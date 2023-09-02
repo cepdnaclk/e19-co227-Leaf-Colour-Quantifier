aa1743927ad0d2853d60a7a9b40de62fd3cd4c95
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-09-02 10:27:02 +0530

    test

--- backend/test/image_processing_test.py
+++ backend/test/image_processing_test.py
@@ -4,15 +4,20 @@ sys.path.insert(0, os.getcwd())
 
 import cv2
 from image_processing.Image import Image
+import numpy as np
  
 # Load the image
 image = cv2.imread("test\\photo1.jpg")
 myImage = Image(image)
 
 def test_image():
-    assert (image == myImage.getImage()).all()
+    if type(image == myImage.getImage()) == np.ndarray:
+        assert (image == myImage.getImage()).all()
+    else:
+        assert image == myImage.getImage()
 
 if __name__ == "__main__":
+    print(type(image == myImage.getImage()) == np.ndarray)
     cv2.imshow("Image", myImage.getGradImage())
     # cv2.imshow("Image", myImage.image)
 
