1f51114c2a59652ba4a969f352da23411551b541
Kanishka Gunawardana <kanishkagunawarthana@gmail.com>
2023-09-20 12:57:19 +0530

    coment the enhanced api

--- backend/rest_api/routes/image.py
+++ backend/rest_api/routes/image.py
@@ -5,12 +5,12 @@ from rest_api.controllers import imageController
 
 router = APIRouter()
 
-@router.post("/enhace", response_class=StreamingResponse)
-async def getEnhacedImage(file: UploadFile = File(...)):
-    contents = await file.read()
+# @router.post("/enhace", response_class=StreamingResponse)
+# async def getEnhacedImage(file: UploadFile = File(...)):
+#     contents = await file.read()
 
-    # Return the streaming response.
-    return imageController.getImageToEnhance(contents)
+#     # Return the streaming response.
+#     return imageController.getImageToEnhance(contents)
 
 @router.post("/segmentaion", response_class=StreamingResponse)
 async def getSegmentationImage(file: UploadFile = File(...)):
