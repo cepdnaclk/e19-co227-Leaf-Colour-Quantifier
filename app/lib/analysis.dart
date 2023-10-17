95f0178c8509e3b0c99854871660478e50c912ed
kaushithamsilva <kaushithamsilva@gmail.com>
2023-10-17 20:08:20 +0530

    Fixed Analysis Reload on Toggle

--- app/lib/analysis.dart
+++ app/lib/analysis.dart
@@ -1,12 +1,8 @@
-import 'dart:collection';
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:leaf_spectrum/components/histogram.dart';
-import 'dart:typed_data';
-import 'package:image/image.dart' as img;
+import 'package:leaf_spectrum/components/histogram_with_toggles.dart';
 import 'package:leaf_spectrum/components/pie_chart.dart';
 import 'package:leaf_spectrum/models/dominant_colors_data.dart';
-import 'package:leaf_spectrum/models/histogram_data.dart';
 import 'package:leaf_spectrum/home.dart';
 import 'package:leaf_spectrum/models/server_connection.dart';
 import 'package:leaf_spectrum/showRemarkDialog.dart';
@@ -29,64 +25,13 @@ class _AnalysisState extends State<Analysis> {
   final File originalImage;
 
   _AnalysisState({required this.imageFile, required this.originalImage});
-  //List to contain the toggle button status of the three channels; RGB in order.
-  List<bool> toggleValues = [true, true, true];
+
 
   Future<DominantColorsData> getDominantColors() async {
     return ServerConnection().getDominantColorsFromImage(imageFile);
   }
 
-  Future<HistogramData> getHistogramMaps() async {
-    final Uint8List imageData = await widget.imageFile.readAsBytesSync();
-    img.Image? image = img.decodeImage(imageData);
 
-    // Split the image into channels
-    List<int> channels = [];
-
-    channels = (await image?.getBytes(order: img.ChannelOrder.rgb))!;
-
-    Map<int, int> redData = {};
-    Map<int, int> blueData = {};
-    Map<int, int> greenData = {};
-
-    //initializing the map
-    for (int i = 0; i < 256; i++) {
-      redData[i] = 0;
-      greenData[i] = 0;
-      blueData[i] = 0;
-    }
-    int max = 0;
-    for (int i = 0; i < channels.length; i += 3) {
-      int red = channels[i];
-      int green = channels[i + 1];
-      int blue = channels[i + 2];
-
-      //Eliminating Black Pixels
-      if (red == 0 && green == 0 && blue == 0) continue;
-
-      //Adding data to Map
-      redData[red] = (redData[red] ?? 0) + 1;
-      greenData[green] = (greenData[green] ?? 0) + 1;
-      blueData[blue] = (blueData[blue] ?? 0) + 1;
-
-      //Calculating the maximum values
-      max = redData[red]! > max ? redData[red]! : max;
-      max = greenData[green]! > max ? greenData[green]! : max;
-      max = blueData[blue]! > max ? blueData[blue]! : max;
-    }
-
-    //sorting the Map according to its key
-    redData = SplayTreeMap<int, int>.from(redData);
-    greenData = SplayTreeMap<int, int>.from(greenData);
-    blueData = SplayTreeMap<int, int>.from(blueData);
-
-    //return HistogramData object
-    return HistogramData([
-      redData,
-      greenData,
-      blueData,
-    ], max);
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -132,78 +77,14 @@ class _AnalysisState extends State<Analysis> {
                           const Color.fromRGBO(64, 72, 80, 0.4196078431372549)),
                   alignment: Alignment.center,
                   clipBehavior: Clip.hardEdge,
-                  child: Column(
-                    children: [
-                      Expanded(
-                        //aspectRatio: 0.9,
-                        child: FutureBuilder<HistogramData>(
-                          future: getHistogramMaps(),
-                          builder: (BuildContext context,
-                              AsyncSnapshot<HistogramData> snapshot) {
-                            if (snapshot.connectionState ==
-                                ConnectionState.waiting) {
-                              return CircularProgressIndicator();
-                            } else if (snapshot.hasError) {
-                              return Text('Error: ${snapshot.error}');
-                            } else {
-                              return Histogram(
-                                histogramData: snapshot.data!,
-                                showRed: toggleValues[0],
-                                showGreen: toggleValues[1],
-                                showBlue: toggleValues[2],
-                              );
-                            }
-                          },
-                        ),
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        "Showing Channels",
-                        style: TextStyle(
-                          color: Color.fromRGBO(255, 255, 255, 0.5),
-                          fontSize: 12,
-                        ),
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      ToggleButtons(
-                        isSelected: toggleValues,
-                        onPressed: (int index) {
-                          setState(() {
-                            toggleValues[index] = !toggleValues[index];
-                          });
-                        },
-                        borderRadius: BorderRadius.circular(30.0),
-                        constraints:
-                            BoxConstraints.expand(width: 80, height: 35),
-                        color: Color.fromRGBO(255, 255, 255, 0.4),
-                        disabledBorderColor: Colors.white24,
-                        children: [
-                          Text(
-                            "Red",
-                            style: TextStyle(fontSize: 12.0),
-                          ),
-                          Text(
-                            "Green",
-                            style: TextStyle(fontSize: 12.0),
-                          ),
-                          Text(
-                            "Blue",
-                            style: TextStyle(fontSize: 12.0),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
+                  child:
+                    HistogramCardWithToggles(imageFile: imageFile,),
                 ),
                 const SizedBox(
                   height: 40,
                 ),
                 Container(
-                  height: 360,
+                  height: 380,
                   padding: const EdgeInsets.symmetric(
                       vertical: 20.0, horizontal: 20.0),
                   decoration: BoxDecoration(
@@ -221,6 +102,7 @@ class _AnalysisState extends State<Analysis> {
                           fontWeight: FontWeight.w600,
                         ),
                       ),
+                      SizedBox(height: 10,),
                       FutureBuilder<DominantColorsData>(
                         future:
                             getDominantColors(), // Assuming getDominantColors() returns a Future<List<Data>>
@@ -233,7 +115,9 @@ class _AnalysisState extends State<Analysis> {
                             return Text('Error: ${snapshot.error}');
                           } else {
                             return PieChartWidget(
-                                dominantColorsData: snapshot.data!);
+                                dominantColorsData: snapshot.data!
+
+                            );
                           }
                         },
                       ),
