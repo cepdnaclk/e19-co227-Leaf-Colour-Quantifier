8d96d525223dbd0557ca86964ebff700c509c59b
Mansitha <iammansitha@gmail.com>
2023-10-17 12:10:03 +0530

    Added a download complete notification

--- app/lib/showRemarkDialog.dart
+++ app/lib/showRemarkDialog.dart
@@ -4,14 +4,23 @@ Future<String?> showRemarkDialog(BuildContext context) async {
   final TextEditingController controller = TextEditingController();
 
   final AlertDialog dialog = AlertDialog(
+    backgroundColor: Color.fromARGB(255, 33, 145, 126),
     title: Text('Remark'),
     content: TextField(
       autofocus: true,
       controller: controller,
-      decoration: InputDecoration(hintText: 'Enter a remark'),
+      cursorColor: Colors.black,
+      style: TextStyle(color: Colors.black),
+      decoration: InputDecoration(
+          focusedBorder:
+              UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+          hintText: 'Enter a remark',
+          hintStyle: TextStyle(color: Colors.black)),
     ),
     actions: [
       TextButton(
+        style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.black)),
         onPressed: () {
           Navigator.of(context).pop(controller.text);
         },
