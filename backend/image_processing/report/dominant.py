e15533999586e93a3fb06fe1e786b3dabdc91997
Sanduni Ubayasiri <115542118+SanduniU@users.noreply.github.com>
2023-10-18 11:13:43 +0530

    add k means clustering to report

---
+++ backend/image_processing/report/dominant.py
@@ -0,0 +1,51 @@
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+
+def get_dominant_colors(image, num_colors=3):
+
+    # Convert the image to RGB color space
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    # Flatten the image into a 2D array of pixels
+    pixels = image_rgb.reshape(-1, 3)
+
+    # Create a boolean mask for rows that are not [0, 0, 0]
+    mask = np.any(pixels != [0, 0, 0], axis=1)
+
+    # Use the mask to filter the array
+    pixels = pixels[mask]
+
+    # Perform K-means clustering
+    kmeans = KMeans(n_clusters=num_colors)
+    kmeans.fit(pixels)
+
+    # Get the labels and counts for each cluster
+    labels, counts = np.unique(kmeans.labels_, return_counts=True)
+    # print(labels, counts)
+
+    # Sort the clusters based on count in descending order
+    sorted_clusters = np.argsort(counts)[::-1]
+    # print(sorted_clusters)
+
+    # Get the top three dominant colors
+    top_clusters = sorted_clusters[:num_colors]
+    # print(top_clusters)
+
+    # Get the RGB values of the top three clusters
+    top_colors = kmeans.cluster_centers_[top_clusters].astype(int)
+    # print(top_colors)
+
+    # Calculate the percentage of each color
+    total_pixels = pixels.shape[0]
+    color_percentages = counts[top_clusters] / total_pixels * 100
+
+    # Calculate the spread of each color
+    color_spreads = np.sqrt(
+        np.mean(np.square(pixels - kmeans.cluster_centers_[kmeans.labels_]), axis=0))
+    
+    top_colors = [[int(x) for x in y] for y in top_colors]
+    color_spreads = [float(x) for x in color_spreads]
+    color_percentages = [float(x) for x in color_percentages]
+
+    return top_colors, color_spreads, color_percentages
