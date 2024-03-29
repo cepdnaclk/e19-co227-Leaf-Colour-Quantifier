ba90f7ef96f6aa512e39f327e28abf7cf083b578
Kanishka Gunawardana <83855665+KATTA-00@users.noreply.github.com>
2024-03-29 11:34:59 +0530

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -4,4 +4,6 @@ source /home/e19co227/anaconda3/etc/profile.d/conda.sh
 
 conda activate leaf_env 
 
-python /home/e19co227/e19-co227-Leaf-Colour-Quantifier/backend/main.py
+cd /home/e19co227/e19-co227-Leaf-Colour-Quantifier/backend
+
+gunicorn rest_api.api:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:5000
